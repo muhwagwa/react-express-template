24ccede19fa7828cc1331a88c238c8e2143438a1
Chloe Na <chloe.na@agilebits.com>
2024-05-27 00:41:19 -0500

    connect register page with backend

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,13 +1,42 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { Link, TextField } from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 function Register() {
+  const navigate = useNavigate();
+  const toLogin=()=>{
+      navigate("/login");
+  }
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+    console.log(username)
+    console.log(password)
+    axios.post('http://localhost:3000/register', {
+        username: username,
+        password: password,
+    })
+    .then(resp => {
+        navigate("/");
+    })
+    .catch(error => {
+        console.log(error);
+        navigate("/register");
+    });
+  }
+
   return (
     <Stack spacing={2} direction="column">
         <h3>Register</h3>
-          <form className="form" noValidate>
+          <form className="form" noValidate onSubmit={handleSubmit}>
             <Stack spacing={1} direction="column">
             <TextField
                 variant="outlined"
@@ -15,6 +44,7 @@ function Register() {
                 id="username"
                 label="Username"
                 name="username"
+                onChange={(e) => setUsername(e.target.value)}
                 autoFocus
             />
             <TextField
@@ -24,6 +54,7 @@ function Register() {
                 label="Password"
                 type="password"
                 id="password"
+                onChange={(e) => setPassword(e.target.value)}
             />
             <TextField
                 variant="outlined"
@@ -32,6 +63,7 @@ function Register() {
                 label="Confirm Password"
                 type="password"
                 id="confirm-password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
             />
             <Button
                 type="submit"
@@ -41,11 +73,11 @@ function Register() {
             >
                 Register
             </Button>
-            <Link href="#" variant="body2">
-                {"Already have an account? Sign in"}
-            </Link>
             </Stack>
           </form>
+          <Link href="login" variant="body2">
+            {"Already have an account? Sign in"}
+          </Link>
     </Stack>
   );
 }
